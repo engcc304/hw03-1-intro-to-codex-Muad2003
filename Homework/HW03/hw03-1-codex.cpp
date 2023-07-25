62f338d06c78a731511d5d27c382d1760c90a422
Muad2003 <anuphap2003118@gmail.com>
2023-07-26 01:18:30 +0700

    update homework 3-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -4,7 +4,7 @@
 int main() {
   int a = 10 ;
   int b = 20, c = 30, d = 40 ;
-  printf( "This is %d + %d = %d " , a, b, a+b );
+  printf( "This is %d + %d = %d " , a, b, a+b ) ;
 
   for( int i = 0 ; i < d-30 ; i++ ) {
     if( i < 30 ) {
