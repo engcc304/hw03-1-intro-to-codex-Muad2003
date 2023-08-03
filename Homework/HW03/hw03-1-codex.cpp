41b02b1e39dc91a21b2bc29fd21573d02a89a248
Muad2003 <anuphap2003118@gmail.com>
2023-08-03 15:07:14 +0700

    update homework 3-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -1,7 +1,9 @@
 //TODO: #8 Arrange this C coding into CODEX pattern.
 #include <stdio.h>
 #include <conio.h>
+
 int main() {
+    
     int a = 10 ;
     int b = 20, c = 30, d = 40 ; 
     printf( "This is %d + %d = %d ", a, b, a + b ) ;
