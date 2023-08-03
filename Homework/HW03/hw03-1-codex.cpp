32eb148a2502b734600d6bb6ea92af3656e3df64
Muad2003 <anuphap2003118@gmail.com>
2023-08-03 15:06:26 +0700

    update homework 3-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -3,7 +3,7 @@
 #include <conio.h>
 int main() {
     int a = 10 ;
-    int b = 20, c = 30, d = 40; 
+    int b = 20, c = 30, d = 40 ; 
     printf( "This is %d + %d = %d ", a, b, a + b ) ;
     for ( int i = 0 ; i < d - 30 ; i++ ) {
     if( i < 30 ) {
